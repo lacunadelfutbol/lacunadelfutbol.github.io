b24ac4e701c564e24da7a3e839321bbeeaa3967a
lacunadelfutbol <dillansolis2@gmail.com>
2026-02-20 12:49:57 -0600

    Add files via upload
    correcion

--- script8.js
+++ script8.js
@@ -9,7 +9,7 @@ document.getElementById("menu-toggle").addEventListener("click", function() {
 });
 
 // ----- CONFIGURACIÓN -----
-const TOTAL_ASISTENCIAS_POSIBLES = 40;
+const TOTAL_ASISTENCIAS_POSIBLES = 41;
 
 // ----- Datos de ejemplo -----
 const jugadores = [
